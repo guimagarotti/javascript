fb14bad9aef9ae5296aac3a62c4fcf57dbd89089
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-03 09:35:52 -0300

    ex008 e ex009

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 59.9
+console.log(`A velocidade do seu carro é de: ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou os limites de velocidade! MULTADO!`)
+}
+console.log(`Dirija sempre com cinto de segurança!`)
---
+++ exercicios/ex009/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Atualmente, você está morando no/na ${país}`)
+if (país != 'Brasil') {
+    console.log(`Você é estrangeiro!`)
+} else {
+    console.log(`Você é brasileiro!`)
+}
