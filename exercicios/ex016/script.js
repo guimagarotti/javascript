d0fac0412a8b384fe7deed88d03989d02a4b7d84
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-08 09:47:49 -0300

    ex016

---
+++ exercicios/ex016/script.js
@@ -0,0 +1,19 @@
+var cont = 1
+while (cont <= 10) {
+    console.log('Olá!')
+    cont++
+}
+
+
+var cont = 1
+do {
+    console.log(`Etapa ${cont}!`)
+    cont++
+} while (cont <= 3)
+
+console.log('Receba!')
+console.log('Receba!')
+console.log('Receba!')
+console.log('Receba!')
+console.log('Receba!')
+
