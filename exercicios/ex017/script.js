76cd55e7cb83a3f5110afebd49a74900596f4ab4
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-11 15:47:43 -0300

    ex017

--- exercicios/ex017/script.js
+++ exercicios/ex017/script.js
@@ -1,7 +1,29 @@
-function gerar() {
-    var nE = Number(window.document.getElementById('txt1'))
-    var res = Number(window.document.getElementById('txt2'))
+function tabuada() {
+    var nI = document.getElementById('txtn')
+    var tab = document.getElementById('seltab')
 
-    
+    if (nI.value.length == 0) {
+        window.alert('[ERRO] Digite um número!')
+    } else {
+        var nF = Number(nI.value)
+        var c = 1
+        tab.innerHTML = ''
+        
+        /*
+        while (c <= 10) {
+            var item = document.createElement('option')            
+            item.text = ` ${nF} x ${c} = ${nF*c}`
+            item.value = `tab${nF}`
+            tab.appendChild(item)
+            c++
+        }
+        */
+
+        for (var c = 1; c <= 10; c++) {
+            var res = document.createElement('option')
+            res.text = `${nF} x ${c} = ${nF*c}`
+            res.value = `tab${nF}`
+            tab.appendChild(res)
+        }
     }
 }
