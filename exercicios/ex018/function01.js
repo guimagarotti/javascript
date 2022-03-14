61708bc964e139a79e579a2a861b1e8e1580e6ca
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-14 16:13:08 -0300

    ex018

---
+++ exercicios/ex018/function01.js
@@ -0,0 +1,9 @@
+function parouimp(n) {
+    if (n % 2 == 0) {
+        return 'PAR!'
+    } else {
+        return 'ÍMPAR!'
+    }
+}
+var res = parouimp(8)
+console.log(res)
---
+++ exercicios/ex018/function03.js
@@ -0,0 +1,5 @@
+var func = function (x) {
+    return x*2
+}
+
+console.log(func(3))
