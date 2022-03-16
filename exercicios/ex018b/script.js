d69e4163b9e05366570fec7e436b9eb89a000429
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-03-16 10:11:48 -0300

    ex018b

--- exercicios/ex018b/script.js
+++ exercicios/ex018b/script.js
@@ -1,12 +1,5 @@
 var vetor = []
-/*
-function eNumero(n) {
-    if (Number(n) >= 1 && Number(n) <= 100) {
-        return true
-    } else {
-        return false
-    }
-}
+var res = document.getElementById('fim')
 
 function naLista(n, lista) {
     if (lista.indexOf(Number(n)) != -1) {
@@ -15,19 +8,42 @@ function naLista(n, lista) {
         return false
     }
 }
-*/
 
-/*
-if (eNumero(numero.value) && !naLista(numero.value, valores)) {
-    window.alert('OK!')
-} else {
-    window.alert('[ERRO] Valor inválido ou já se encontra na lista!')
-}
-*/
 function adicionar() {
-    if (numero.length == 0 || valores.indexOf(Number(numero)) == -1 || numero >= 1 || numero <= 100) {
-        window.alert('[ERRO] Valor inválido ou já se encontra na lista!')
+    let numero = document.getElementById('n1')
+    let tabela = document.getElementById('seltab')
+    let num = numero.value
+    
+    if (num < 1 || num > 100) {
+        window.alert('[ERRO] Digite um valor válido!')
+    } else if (!naLista(num, vetor)) {
+        vetor.push(Number(num))
+        let item = document.createElement('option')
+        item.text = `Valor ${Number(num)} adicionado.`
+        tabela.appendChild(item)
+        res.innerHTML = ''
     }
 }
 
+function finalizar() {
+    if (vetor.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+        let totN = vetor.length
+        let maiorN = Math.max.apply(Math, vetor)
+        let menorN = Math.min.apply(Math, vetor)
+        let somaN = 0
+        let medN = 0
+        for (let pos in vetor) {
+            somaN += vetor[pos]
+        }
+        medN = somaN / totN
 
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos <strong>${totN}</strong> números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi <strong>${maiorN}</strong>.</p>`
+        res.innerHTML += `<p>O menor valor informado foi <strong>${menorN}</strong>.</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos <strong>${somaN}</strong>.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é <strong>${Math.round(medN)}</strong>.</p>`
+    }
+}
