9438369f85fd578a2eaadf869fe228ea64515254
Guilherme Magarotti <96914918+guimagarotti@users.noreply.github.com>
2022-04-01 13:21:03 -0300

    dev004

---
+++ exercicios/dev004/main.js
@@ -0,0 +1,13 @@
+const car = 'Ferrari'
+
+switch (car) {
+    case 'Mercedes':
+        console.log("Mercedes é lindo!")
+        break
+    case 'Ferrari':
+        console.log("Ferrari é muito vermelha!")
+        break
+    case 'Tesla':
+        console.log("Tesla é muito elétrico!")
+        break
+}
